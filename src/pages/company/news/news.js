92afcbd2039ac7f43347b91e267c59a4f1f2572b
Max Geller <mxgeller@gmail.com>
2020-10-04 14:07:53 -0700

    tracking netlify issues

--- src/pages/company/news/news.js
+++ src/pages/company/news/news.js
@@ -1,14 +1,13 @@
 import React from "react";
 
-import PageWrapper from "../../components/PageWrapper";
-import Hero from "../../sections/Hero";
-import Fact from "../../sections/Fact";
-import Services from "../../sections/Services";
-import Content from "../../sections/Content";
-import Alert from "../../sections/Alert";
-import Testimonial from "../../sections/Testimonial";
-import CTA from "../../sections/CTA";
-import Newsletter from "../../sections/Newsletter";
+import PageWrapper from "./../../../components/PageWrapper";
+import Fact from "../../../sections/Fact";
+import Services from "../../../sections/Services";
+import Content from "../../../sections/Content";
+import Alert from "../../../sections/Alert";
+import Testimonial from "../../../sections/Testimonial";
+import CTA from "../../../sections/CTA";
+import Newsletter from "../../../sections/Newsletter";
 
 const Consultation = () => {
   return (
