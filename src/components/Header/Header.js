ab85941d1bf340383c793c9e8e6fb44bea94f78c
Max Geller <mxgeller@gmail.com>
2020-11-14 14:56:30 -0700

    updates to header redirect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -227,7 +227,7 @@ const Header = () => {
             {gContext.header.button === "cta" && (
               <div className="header-btn ml-auto ml-lg-0 mr-6 mr-lg-0 d-none d-xs-block">
                 <a
-                  href="https://client.presidiomep.com"
+                  href="https://client.presidiosolutions.com"
                   target="_blank"
                   className={`btn btn-${gContext.header.variant}`}
                 >
