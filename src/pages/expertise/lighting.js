8613cef5ec41e2286fd3828e157174e52b3fedbb
Max Geller <mxgeller@gmail.com>
2020-11-18 10:29:35 -0700

    updating lighting page

--- src/pages/expertise/lighting.js
+++ src/pages/expertise/lighting.js
@@ -6,8 +6,7 @@ import CountUp from "react-countup";
 import LazyLoad from "react-lazyload";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
-import map from "./../../assets/image/stock/map.jpeg";
-
+import map from "./../../assets/image/lighting.jpeg";
 
 const Lighting = () => {
   return (
@@ -25,84 +24,38 @@ const Lighting = () => {
         }}
       >
         {/* <!-- Hero Area --> */}
-    <div
-        className="bg-image bg-gradient bg-gradient-black mt-17 pt-14 pt-lg-12 pb-lg-12"
-        css={`
-          background-image: url(${map});
-        `}
-      >
-        <Container>
-          <Row className="align-items-center">
-            <Col
-              md="9"
-              lg="7"
-              xl="6"
-              data-aos="fade-right"
-              data-aos-duration="750"
-              data-aos-delay="500"
-            >
-              <div className="hero-content section-title dark-mode-texts">
-                <h1 className="gr-text-3 font-weight-bold mb-8">
-                  LIGHTING & CONTROLS
-                </h1>
-                <p className="gr-text-8 pr-sm-7 pr-md-12">
-                History shows the buyout process of MEP systems is the single-largest contributor to trade bid variance. Let's get you setup for success.
-                </p>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+        <div
+          className="bg-image bg-gradient bg-gradient-black mt-17 pt-14 pt-lg-12 pb-lg-12"
+          css={`
+            background-image: url(${map});
+          `}
+        >
+          <Container>
+            <Row className="align-items-center">
+              <Col
+                md="9"
+                lg="7"
+                xl="6"
+                data-aos="fade-right"
+                data-aos-duration="750"
+                data-aos-delay="500"
+              >
+                <div className="hero-content section-title dark-mode-texts">
+                  <h1 className="gr-text-3 font-weight-bold mb-8">
+                    LIGHTING & CONTROLS
+                  </h1>
+                  <p className="gr-text-8 pr-sm-7 pr-md-12">
+                    Lighting and associated controls are a significant
+                    contributor to modern building systems cost. Understanding
+                    material pricing and productivity are crucial when evaluting
+                    lighting systems.
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
 
-
-      {/* <!-- Fact Area --> */}
-      <div className="fact-section pt-12 pb-6 pt-lg-19 pb-lg-17 border-bottom border-gray-3 bg-default-6 ">
-        <Container>
-
-
-              <div className="single-fact d-flex px-md-5 px-lg-2 px-xl-9">
-                <h3 className="gr-text-3 mr-9">1M+</h3>
-                <p className="gr-text-9 mb-0">
-                  Somin somin
-                </p>
-              </div>
-
-            <Col lg="4" md="6" sm="6" className="mb-9 mb-lg-0">
-              <div className="single-fact d-flex px-md-5 px-lg-2 px-xl-9">
-                <h3 className="gr-text-3 mr-9">
-                  <LazyLoad>
-                    <span className="counter">
-                      <CountUp duration={3} end={93} />%
-                    </span>
-                  </LazyLoad>
-                </h3>
-                <p className="gr-text-9 mb-0">
-                  Satisfaction rate from our customers.
-                </p>
-              </div>
-            </Col>
-            <Col lg="4" md="6" sm="6" className="mb-9 mb-lg-0">
-              <div className="single-fact d-flex px-md-5 px-lg-2 px-xl-9">
-                <h3 className="gr-text-3 mr-9">
-                  <LazyLoad>
-                    <span className="counter">
-                      <CountUp
-                        duration={3}
-                        decimal="."
-                        decimals={1}
-                        end={4.9}
-                      />
-                    </span>
-                  </LazyLoad>
-                </h3>
-                <p className="gr-text-9 mb-0">
-                  Average customer ratings out of 5.00!
-                </p>
-              </div>
-            </Col>
-
-        </Container>
-      </div>
         <CTA />
       </PageWrapper>
     </>
