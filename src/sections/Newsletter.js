1d2a9b3cdb9ed3f9cabbd5b752007a6decc45491
Max Geller <mxgeller@gmail.com>
2020-11-22 11:40:08 -0700

    adding mailchimp

--- src/sections/Newsletter.js
+++ src/sections/Newsletter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import Mailchimp from "react-mailchimp-form";
 
 const Newsletter = () => {
   return (
@@ -8,29 +9,26 @@ const Newsletter = () => {
       <div className="newsltr-section pt-12 pb-10 pt-lg-19 pb-lg-17 bg-default-6 border-bottom ">
         <Container>
           <Row className="align-items-center justify-content-center">
-            <Col lg="6" md="10" className="pr-lg-9">
+            <Col lg="8" md="10" className="pr-lg-9">
               <div className="nwsltr-text">
                 <h2 className="title gr-text-5">
                   Subscribe to our newsletter to get latest news on your inbox.
                 </h2>
               </div>
             </Col>
-            <Col lg="6" md="10" className="pl-xl-13">
-              <form className="newsletter-form d-flex justify-content-lg-end mt-6 mt-lg-0">
-                <div className="form-group mr-7 w-100">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="leadEmail1"
-                    placeholder="Enter your email"
-                  />
-                </div>
-                <div className="cta-btn">
-                  <Button type="submit" className="with-icon gr-hover-y">
-                    Subscribe<i className="icon icon-tail-right"></i>
-                  </Button>
-                </div>
-              </form>
+            <Col lg="5" md="6" className="pl-xl-13">
+              <Mailchimp
+                className="newsletter-form d-flex justify-content-lg-end mt-6 mt-lg-0"
+                action="https://presidiosolutions.us7.list-manage.com/subscribe/post?u=9ce0509faca32f2bcbdecface&amp;id=8d73c48dce"
+                fields={[
+                  {
+                    name: "EMAIL",
+                    placeholder: "Email",
+                    type: "email",
+                    required: true,
+                  },
+                ]}
+              />
             </Col>
           </Row>
         </Container>
